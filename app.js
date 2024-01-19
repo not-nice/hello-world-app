8cb5195a28fd574f1f3e8b785be321b04ba5448b
not-nice <85105948+not-nice@users.noreply.github.com>
2024-01-19 23:19:09 +0100

    last

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("Hello, World!");
+const AWS = require('aws-sdk');
+
+// Configure AWS SDK with your credentials and region
+AWS.config.update({
+  accessKeyId: '094423642169',
+  region: 'us-east-1',
+});
+
+// Create an RDS client
+const rds = new AWS.RDS();
+
+// Use the RDS client to interact with your database
+// Example: Retrieve data from the RDS instance
+rds.describeDBInstances({}, (err, data) => {
+  if (err) console.error(err);
+  else console.log(data);
+});
+
+console.log("Hello, World!");
